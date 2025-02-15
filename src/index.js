2aabd4907a9c076144c526caf1d3d6c612851b4b
sahandedc <sahandedc@gmail.com>
2025-02-15 18:34:16 +0330

    test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
-// import Datas from "./Datas";
-import Test from "./test";
-import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <BrowserRouter>
-      {/* <Datas /> */}
-      <Test/>
-    </BrowserRouter>
-);
+root.render(<App />);
 reportWebVitals();
