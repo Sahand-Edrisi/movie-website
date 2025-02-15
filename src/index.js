42c58792983092d5dc2338455e79338c0cde7194
sahandedc <sahandedc@gmail.com>
2025-02-15 18:37:32 +0330

    test2

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
+import Test2 from "./test";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+<Test2 />
+);
 reportWebVitals();
