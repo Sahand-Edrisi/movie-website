592654d0e5195deeacf66d04201c9449ee065e71
sahandedc <sahandedc@gmail.com>
2025-02-15 18:22:28 +0330

    test

--- src/index.js
+++ src/index.js
@@ -5,10 +5,8 @@ import Datas from "./Datas";
 import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
     <BrowserRouter>
       <Datas />
     </BrowserRouter>
-  </React.StrictMode>
 );
 reportWebVitals();
