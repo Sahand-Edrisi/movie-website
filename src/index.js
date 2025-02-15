137d5866566c8d51f1f681595b1de96d3f99ba5a
sahandedc <sahandedc@gmail.com>
2025-02-15 18:11:11 +0330

    Fix : update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
 import Datas from "./Datas";
 import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <React.StrictMode>
     <BrowserRouter>
-<Datas/>
+      <Datas />
     </BrowserRouter>
+  </React.StrictMode>
 );
 reportWebVitals();
